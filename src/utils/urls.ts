b3e58b89102a3584dded184a0c848f3ad60ccbe7
AndersonGomezGarcia <Gomez.anderson@correounivalle.edu.co>
2025-08-31 16:52:42 -0500

    fix paths, add more translate terms, update list of images weekart

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,5 +1,5 @@
-//export const globalUrl = 'http://localhost:4321/';// 'https://andersongomezgarcia.github.io/portfolio/';
 export const globalUrl = 'https://andersongomezgarcia.github.io/portfolio/';
+//export const globalUrl = 'http://localhost:4321/portfolio/';
 const baseUrl = 'img/icons/';
 
 const url = {
